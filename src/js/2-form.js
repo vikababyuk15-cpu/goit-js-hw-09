3ca579384a427a406dfe0418d8f93d272bea76d9
Vika <vikababyuk15@gmail.com>
2026-01-15 15:21:25 +0200

    edited

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -2,6 +2,16 @@ const form = document.querySelector('.feedback-form');
 const localStorageKey = "feedback-form-state";
 
 let formData = { email:"", message:"" };
+const savedData = localStorage.getItem(localStorageKey);
+if (savedData) {
+  const parsedData = JSON.parse(savedData);
+  formData = parsedData;
+for (const key in formData) {
+    if (formData.hasOwnProperty(key)) {
+      form.elements[key].value = formData[key];
+    }
+  }
+}
 form.addEventListener('input', (event) => 
 {const { name, value } = event.target;
   
